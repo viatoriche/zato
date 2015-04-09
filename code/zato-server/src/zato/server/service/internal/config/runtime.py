27530ce02cef37a4ed5e746a4587ffa5e90f9c12
Dariusz Suchojad <dsuch-github@m.zato.io>
2015-04-09 22:48:18 +0200

    GH #416 - List both static files and those from pick up dirs.

--- code/zato-server/src/zato/server/service/internal/config/runtime.py
+++ code/zato-server/src/zato/server/service/internal/config/runtime.py
@@ -26,23 +26,40 @@ class GetList(AdminService):
         response_elem = 'zato_config_runtime_get_list_response'
         input_required = ('cluster_id',)
         output_required = ('name',)
-        output_optional = ('full_path', 'access_rights')
+        output_optional = ('pickup_dir', 'full_path', 'access_rights')
         output_repeated = True
 
+    def _get_info(self, path, parent_dir=None):
+
+        if not p.isabs(path):
+            full_path = p.abspath(p.normpath(p.join(parent_dir or self.server.repo_location, path)))
+        else:
+            full_path = path
+
+        try:
+            access_rights = oct(S_IMODE(os.stat(full_path).st_mode))
+        except OSError, e:
+            self.logger.warn('Could not stat `%s` (%s), e:`%s`', full_path, path, format_exc(e))
+            access_rights = None
+
+        return full_path, access_rights
+
+    def _append(self, name, pickup_dir, full_path, access_rights, ):
+
+        self.response.payload.append({
+            'name':name, 'full_path':full_path, 'access_rights':access_rights, 'pickup_dir':pickup_dir
+        })
+
     def handle(self):
-        for name, path in sorted(self.server.fs_server_config.runtime_config.iteritems()):
+        for name, item in sorted(self.server.fs_server_config.runtime_config.iteritems()):
+
+            # Regular static files
             if name != 'pickup_dir':
+                self._append(name, None, *self._get_info(item))
 
-                if not p.isabs(path):
-                    full_path = p.abspath(p.normpath(p.join(self.server.repo_location, path)))
-                else:
-                    full_path = path
-
-                try:
-                    access_rights = oct(S_IMODE(os.stat(full_path).st_mode))
-                except OSError, e:
-                    self.logger.warn('Could not stat `%s` (%s), e:`%s`', full_path, name, format_exc(e))
-                    access_rights = None
-
-                self.response.payload.append({'name':name, 'full_path':full_path, 'access_rights':access_rights})
-
+            # Paths to pick up dirs (but can be just a single path as well)
+            else:
+                for dir_path in item if isinstance(item, list) else [item]:
+                    full_pickup_dir = p.join(self.server.repo_location, dir_path)
+                    for path in os.listdir(full_pickup_dir):
+                        self._append(path, dir_path, *self._get_info(path, full_pickup_dir))
